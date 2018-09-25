c5524566deec4e688f708ea019a08083ce975745
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2018-09-24 20:17:53 -0700

    allow running from different root

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,19 @@
+extern crate nd_lib;
+
 #[macro_use]
 extern crate clap;
-extern crate nd_lib;
+
+#[macro_use]
+extern crate log;
+extern crate env_logger;
 
 use clap::{App, Arg, SubCommand};
 use std::process::{exit, Command};
 
 use nd_lib::package::Package;
 
-fn exec(args: Vec<&str>) {
-    let pkg = Package::load("../nd_lib/fixtures/3-dep-not-installed");
+fn exec(root: &str, args: Vec<&str>) {
+    let pkg = Package::load(root);
     for issue in pkg.validate() {
         println!("{:?}", issue);
     }
@@ -20,6 +25,7 @@ fn exec(args: Vec<&str>) {
 }
 
 fn main() {
+    env_logger::init();
     let mut app = App::new("nd")
         .version(crate_version!())
         // .author(crate_authors!())
@@ -29,14 +35,23 @@ fn main() {
                 .aliases(&["x"])
                 .about("run node script directly (equivalent to running node directly)")
                 .arg(Arg::with_name("args")
+                     .required(true)
                      .multiple(true))
+        )
+        .arg(Arg::with_name("root")
+             .short("r")
+             .long("root")
+             .value_name("DIR")
+             .help("Set project root")
+             .takes_value(true)
         );
     let matches = app.clone().get_matches();
-    // println!("{:?}", matches);
+    let root = matches.value_of("root").unwrap_or("");
+    info!("{:?}", matches);
     match matches.subcommand_name() {
         Some("exec") => {
             let matches = matches.subcommand_matches("exec").unwrap();
-            exec(matches.values_of("args").unwrap().collect());
+            exec(root, matches.values_of("args").unwrap().collect());
         }
         None => {
             app.print_long_help().unwrap();
